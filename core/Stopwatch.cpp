195a711cf3e6158ca6b1e09e1b25bd73a0e2baa6
Kyan Van den Eynde <kyanvde@gmail.com>
2026-07-08 20:16:58 +0000

    fix game tick not being updated

---
+++ core/Stopwatch.cpp
@@ -0,0 +1,19 @@
+#include "Stopwatch.h"
+
+namespace core {
+
+Stopwatch& Stopwatch::getInstance() noexcept {
+    static Stopwatch instance;
+    return instance;
+}
+
+void Stopwatch::tick() noexcept {
+    const std::chrono::high_resolution_clock::time_point currentTime = std::chrono::high_resolution_clock::now();
+    const std::chrono::duration<float> elapsedTime = currentTime - lastTime;
+    deltaTime = elapsedTime.count();
+    lastTime = currentTime;
+}
+
+float Stopwatch::getDeltaTime() const noexcept { return deltaTime; }
+
+} // namespace core
